de0140a362938b131e7a893b2df454e997917d9d
Jayser Mendez <jayser.mendez@stu.bcc.cuny.edu>
2018-03-26 04:13:48 -0400

    indentation fixed for auth.js

--- src/imports/store/auth.js
+++ src/imports/store/auth.js
@@ -9,44 +9,30 @@ const authModule = {
 		updateUser(state, value) {
 			state.user = value
 		},
-    isUsernameValid(state, value) {
-      state.isUsernameValid = value
-    }
+    	isUsernameValid(state, value) {
+      		state.isUsernameValid = value
+    	}
 	},
 	actions: {
 		submitRegisterForm({commit, state}, formData) {
 			Accounts.createUser(formData, error => {
-			error ? console.log(error.reason) : console.log('user registered')
+				error ? console.log(error.reason) : console.log('user registered')
 			})
 		},
 		submitLoginForm({commit, state}, formData) {
-				return new Promise(resolve => {
-					Meteor.loginWithPassword(formData.username, formData.password, error => {
-						if (error) {
-							resolve({
-								status: false,
-								message: error.reason
-							});
-						}
-						else {
-							resolve({
-								status: true,
-								message: 'user logged in'
-							});
-						}
-					})
-				})
+			return new Promise(resolve => {
+				Meteor.loginWithPassword(formData.username, formData.password, error => {
+					error ? resolve({status: false, message: error.reason}) : resolve({status: true, message: 'user logged in'});
+				});
+			});
 		
 		},
 		logout() {
 			return new Promise(resolve => {
 				Meteor.logout(() => {
-					resolve({
-						status: true,
-						message: 'user logged out'
-					});
-				})
-			})
+					resolve({status: true, message: 'user logged out'});
+				});
+			});
 		}
 	}
 }
